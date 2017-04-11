e2d002b62ab27ba204a6e06d77b138b8745cac9b
xiaoya0105 <gracezheng1993@gmail.com>
2017-04-11 16:14:49 -0400

    make some changes of duration density

--- Group2Workspace/graph-final.R
+++ Group2Workspace/graph-final.R
@@ -7,11 +7,20 @@ ggplot(FamilyFinalData, aes(x=CloseTimes, fill=BasicNeeds)) +
 ggplot(FamilyFinalData, aes(x=CloseTimes, fill=FSC)) + 
   geom_density(alpha=.6)
 
-ggplot(FamilyFinalData, aes(x=Duration, fill=Housing)) + 
-  geom_density(alpha=.6)
+ggplot(FamilyFinalData, aes(x=Duration, fill=Housing,color=Housing)) + 
+  geom_density(alpha=.6)+
+  guides(color=FALSE)+
+  ggtitle("Duration and Housing")+
+  scale_fill_discrete(name = "Housing",labels = c("Post-CYF Servie and NA", "Pre-CYF Service"))
 
-ggplot(FamilyFinalData, aes(x=Duration, fill=BasicNeeds)) + 
-  geom_density(alpha=.6)
+ggplot(FamilyFinalData, aes(x=Duration, fill=BasicNeeds,color=BasicNeeds)) + 
+  geom_density(alpha=.6)+
+  guides(color=FALSE)+
+  ggtitle("Duration and Basic Needs")+
+  scale_fill_discrete(name = "Basic Needs",labels = c("Post-CYF Servie and NA", "Pre-CYF Service"))
 
-ggplot(FamilyFinalData, aes(x=Duration, fill=FSC)) + 
-  geom_density(alpha=.6)
+ggplot(FamilyFinalData, aes(x=Duration, fill=FSC,color=FSC)) + 
+  geom_density(alpha=.6)+
+  guides(color=FALSE)+
+  ggtitle("Duration and FSC")+
+  scale_fill_discrete(name = "FSC",labels = c("Post-CYF Servie and NA", "Pre-CYF Service"))
