ddf36a13eca7fd6949ea67dbe4a4aaf3494b1ca2
alberto-guzman <alvarez1991@gmail.com>
2017-04-11 16:36:22 -0400

    done

--- Group2Workspace/graph-final.R
+++ Group2Workspace/graph-final.R
@@ -1,18 +1,15 @@
 
 
 library(readr)
-FamilyFinalData <- read_csv("~/DHS_We-can-do-it/FamilyFinalData.csv")
-View(FamilyFinalData)
-
 library(ggplot2)
-
-
 library(plyr)
+FamilyFinalData <- read_csv("~/DHS_We-can-do-it/FamilyFinalData.csv")
+
+
+
+
+##
 mu <- ddply(FamilyFinalData, "Housing", summarise, grp.mean=mean(CloseTimes))
-
-
-
-
 
 ggplot(FamilyFinalData, aes(x=CloseTimes, fill=Housing,color=Housing)) + 
   geom_histogram(aes(y = ..density..),binwidth=.5, position="dodge", alpha=0.5)+
@@ -25,10 +22,12 @@ ggplot(FamilyFinalData, aes(x=CloseTimes, fill=Housing,color=Housing)) +
                       labels=c("Post-CYF Service & NA", "Pre-CYF Service")) +
   guides(color=FALSE)
 
+##
+mu <- ddply(FamilyFinalData, "BasicNeeds", summarise, grp.mean=mean(CloseTimes))
 
-ggplot(FamilyFinalData, aes(x=CloseTimes, fill=Housing,color=Housing)) + 
+ggplot(FamilyFinalData, aes(x=CloseTimes, fill=BasicNeeds,color=BasicNeeds)) + 
   geom_histogram(aes(y = ..density..),binwidth=.5, position="dodge", alpha=0.5)+
-  geom_vline(data=mu, aes(xintercept=grp.mean, color=Housing),
+  geom_vline(data=mu, aes(xintercept=grp.mean, color=BasicNeeds),
              linetype="dashed") +
   stat_function(fun = dnorm, colour = "Black",
                 args = list(mean = mean(FamilyFinalData$CloseTimes, na.rm = TRUE),
@@ -37,13 +36,19 @@ ggplot(FamilyFinalData, aes(x=CloseTimes, fill=Housing,color=Housing)) +
                       labels=c("Post-CYF Service & NA", "Pre-CYF Service")) +
   guides(color=FALSE)
 
+##
+mu <- ddply(FamilyFinalData, "FSC", summarise, grp.mean=mean(CloseTimes))
 
-
-ggplot(FamilyFinalData, aes(x=CloseTimes, fill=BasicNeeds)) + 
-  geom_density(alpha=.6)
-
-ggplot(FamilyFinalData, aes(x=CloseTimes, fill=FSC)) + 
-  geom_density(alpha=.6)
+ggplot(FamilyFinalData, aes(x=CloseTimes, fill=FSC,color=FSC)) + 
+  geom_histogram(aes(y = ..density..),binwidth=.5, position="dodge", alpha=0.5)+
+  geom_vline(data=mu, aes(xintercept=grp.mean, color=FSC),
+             linetype="dashed") +
+  stat_function(fun = dnorm, colour = "Black",
+                args = list(mean = mean(FamilyFinalData$CloseTimes, na.rm = TRUE),
+                            sd = sd(FamilyFinalData$CloseTimes, na.rm = TRUE)))+
+  scale_fill_discrete(breaks=c("FALSE", "TRUE"),
+                      labels=c("Post-CYF Service & NA", "Pre-CYF Service")) +
+  guides(color=FALSE)
 
 
 
